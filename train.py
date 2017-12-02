07b336c6d32917a43a7e7881f47bc44edb577009
SnoW246 <SnoW246@azet.sk>
2017-12-02 22:18:35 +0000

    Loading MNIST Dataset
    Includes few information messages for user to inform about the action being done

--- train.py
+++ train.py
@@ -13,3 +13,11 @@ from keras.datasets import mnist
 from keras.models import Sequential, load_model
 from keras.layers.core import Dense, Dropout, Activation
 from keras.utils import np_utils
+
+# Display message to inform user that system will load dataset
+print("Loading MNIST Dataset...")
+# Loading MNIST dataset onto the system, which will automatically download the dataset from
+# the following location: https://s3.amazonaws.com/img-datasets/mnist.npz if not already downloaded
+(X_train, Y_train), (X_test, Y_test) = mnist.load_data()
+# Display message to inform user that loading is done
+print("Loading Complete.")
