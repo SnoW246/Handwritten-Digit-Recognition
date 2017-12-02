298f4e70176e3121bdfa24f3fd6ae81b0905a8a7
SnoW246 <SnoW246@azet.sk>
2017-12-02 16:26:11 +0000

    Initial flask application
    Creation of simple flask application with initial file upload template that runs on port 5000

---
+++ app.py
@@ -0,0 +1,17 @@
+# 		Author: Adrian Golias
+# Adapted from: http://flask.pocoo.org/
+# 				http://flask.pocoo.org/docs/0.12/patterns/fileuploads/
+
+import flask as fl
+from flask import render_template, g, request 
+
+app = fl.Flask(__name__)
+
+@app.route("/")
+
+def name():
+	return render_template("Index.html")
+	
+	
+if __name__ == "__main__":
+	app.run()
